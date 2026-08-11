a5398816af8a11fc7e09464834cdc48d87fd6ae9
karysfontana <karysfontana@gmail.com>
2026-08-11 11:34:16 -0400

    update logo size

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -86,7 +86,7 @@ def SideBarLinks(show_home=False):
     """
 
     # Logo appears at the top of the sidebar on every page
-    st.sidebar.image("assets/logo.png", width=150)
+    st.sidebar.image("assets/logo.png", width=650)
 
     # If no one is logged in, send them to the Home (login) page
     if "authenticated" not in st.session_state:
