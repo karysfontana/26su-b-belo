88a35440bc745f17eff590ad6fa0a24d42d4056d
Nora Harr <harr.n@northeastern.edu>
2026-08-12 08:32:09 -0400

    worked on customer home

--- app/src/pages/00_Customer_Home.py
+++ app/src/pages/00_Customer_Home.py
@@ -21,3 +21,23 @@ if st.button('View My Reservations',
              type='primary',
              use_container_width=True):
     st.switch_page('pages/02_Customer_Reservations.py')
+
+if st.button('View My Profile',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/03_Customer_Profile.py')
+
+if st.button('Search Reviews',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/04_Customer_Reviews.py')
+
+if st.button('Create a New Review',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/05_Customer_Create_Review.py')
+
+if st.button('Edit a Review',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/05_Customer_Edit_Review.py')
