70475a22ee8e17b4dfc7acc4a340e8f6b9eb5baa
nrichie201 <nrichie201@gmail.com>
2026-08-13 10:58:49 -0400

    I am going insane

--- database-files/01_schema.sql
+++ database-files/01_schema.sql
@@ -144,7 +144,7 @@ CREATE TABLE Reviews (
 
 DROP TABLE IF EXISTS Rating;
 CREATE TABLE Rating (
-   RatingID INT AUTO_INCREMENT PRIMARY KEY,
+   RatingID INT AUTO_INCREMENT,
    reviewID INT,
    rate INT UNSIGNED NOT NULL,
    ratingType varchar(7) NOT NULL,
